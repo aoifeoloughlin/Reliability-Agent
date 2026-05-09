1694cae387e65ac640dadf583319e5fc1f66e846
aoifeoloughlin <you@example.com>
2026-05-09 08:40:02 +0100

    #01_02-Scheduler multi threading working

--- reliability_agent/agent/scheduler.py
+++ reliability_agent/agent/scheduler.py
@@ -1,24 +1,27 @@
 import time, logging, signal, threading
 
 class Scheduler:
-    def __init__(self, running, interval, thread):
+    def __init__(self, running, interval):
         self.running = running
         self.interval = interval
-        self.thread = thread
-
-    def stop(self):
-        # This method is called from main.py for the graceful shut down
-        self.running = False
-        logging.info("Scheduler has been stopped...")
     
     def futureTask(self):
         print("Hello from future task")
 
-    def run(self):
-        while self.running:
-            self.thread.wait()
-            print("Scheduler: Signal received, starting work!")
-            self.futureTask()
-            self.thread.clear()
-            
+    def run(self, run_thread, stop_thread):
+        while not stop_thread.is_set():
+            try:
+                thread = run_thread.wait(timeout=0.5)
+
+                if stop_thread.is_set():
+                    break
+
+                if thread:
+                    run_thread.clear()
+                    print("Scheduler: Signal received, starting work!")
+                    self.futureTask()
+                    
+            except Exception as e:
+                print(f"Worker crashed with error: {e}")
+                
         
