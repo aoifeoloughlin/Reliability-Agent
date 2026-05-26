5da986f2cf34ae2bf3cc4ac51b95518e6b32f9dc
aoifeoloughlin <you@example.com>
2026-05-26 13:16:43 +0100

    #01_03 checking cpu usage var

--- reliability_agent/agent/collectors/cpu.sh
+++ reliability_agent/agent/collectors/cpu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #sudo +x agent/collectors/cpu.sh - need to make this a sub issue
 read -r cpu user nice system idle iowait irq softirq _ < <(grep '^cpu ' /proc/stat)
-total_idle = $(idle + iowait)
-total_active = $(user+nice+system+idle+iowait+irq+softirq+steal)
+total_idle = $((idle + iowait))
+total_active = $((user + nice + system + idle + irq + softirq))
 echo "total $total_active"
 cpu_usage=$((100 * ((total_active-total_idle)/total_active)))
 echo "CPU_Usage: $cpu_usage %"
