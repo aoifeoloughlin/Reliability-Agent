a899545354e63020db99d8c7c343495398dc2d60
aoifeoloughlin <you@example.com>
2026-05-26 13:19:52 +0100

    #01_03 declaring vars

--- reliability_agent/agent/collectors/cpu.sh
+++ reliability_agent/agent/collectors/cpu.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #sudo +x agent/collectors/cpu.sh - need to make this a sub issue
+local total_idle, total_active, cpu_usage
 read -r cpu user nice system idle iowait irq softirq _ < <(grep '^cpu ' /proc/stat)
 total_idle = $((idle + iowait))
 total_active = $((user + nice + system + idle + irq + softirq))
