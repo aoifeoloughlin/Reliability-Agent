7d6e67cc18c79c0ba6f615cb511778ffa8fb4d8f
aoifeoloughlin <you@example.com>
2026-05-26 12:12:13 +0100

    #01_03 debugging missing percent val

--- reliability_agent/agent/collectors/cpu.sh
+++ reliability_agent/agent/collectors/cpu.sh
@@ -3,6 +3,6 @@ chmod +x agent/collectors/cpu.sh
 read -r _ user nice system idle iowait irq softirq steal _ < /proc/stat
 total_idle = $((idle + iowait))
 total_active = $((user+nice+system+idle+iowait+irq+softirq+steal))
-
+echo $total_active
 cpu_usage=$((100 * ((total_active-total_idle)/total_active)))
 echo "CPU_Usage: $cpu_usage%"
