3e67729bffab4168383c33eaeafe6d1d3ef3f705
aoifeoloughlin <you@example.com>
2026-05-26 13:54:58 +0100

    #01_03 declaring vars

--- reliability_agent/agent/collectors/cpu.sh
+++ reliability_agent/agent/collectors/cpu.sh
@@ -2,8 +2,8 @@
 #sudo +x agent/collectors/cpu.sh - need to make this a sub issue
 local total_idle total_active cpu_usage
 read -r cpu user nice system idle iowait irq softirq _ < <(grep '^cpu ' /proc/stat)
-total_idle = $((idle + iowait))
-total_active = $((user + nice + system + idle + irq + softirq))
-echo "total $total_active"
-cpu_usage=$((100 * ((total_active-total_idle)/total_active)))
-echo "CPU_Usage: $cpu_usage %"
+total_idle=$((idle + iowait))
+total_active=$((user + nice + system + idle + irq + softirq))
+echo "total: $total_active"
+cpu_usage=$(( 100 * (total_active - total_idle) / total_active ))
+echo "CPU Usage: $cpu_usage %"
