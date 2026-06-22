53d49b9c4a66ae4fa2c0f9008b80b3ef229066b1
aoifeoloughlin <you@example.com>
2026-06-22 10:30:14 +0100

    #01_03 added main logging and tick count

--- reliability_agent/agent/scheduler.py
+++ reliability_agent/agent/scheduler.py
@@ -4,19 +4,24 @@ class Scheduler:
     def __init__(self, running, interval):
         self.running = running
         self.interval = interval
+        self.tick_count = 0
     
     def futureTask(self):
-        print("Hello from future task")
+        logging.info("Hello from future task")
 
     def run(self, run_thread, stop_thread):
         while not stop_thread.is_set():
+            self.tick_count += 1
             try:
                 thread = run_thread.wait(timeout=0.5)
                 if stop_thread.is_set():
                     break
                 if thread:
                     run_thread.clear()
-                    print("Scheduler: Signal received, starting work!")
+                    logging.info("Scheduler: Signal received, starting work!")
+                    start=time.monotonic()
                     self.futureTask()  
+                    duration = time.monotonic()-start
+                    logging.info(f"Tick {self.tick_count} completed in {duration:.2f}s", extra={"duration"=duration, "tick_count"=self.tick_count})
             except Exception as e:
-                print(f"Worker crashed with error: {e}")
+                logging.error(f"Worker crashed with error: {e}")
