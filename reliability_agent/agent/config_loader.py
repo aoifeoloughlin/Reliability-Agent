1208d6e4db182f30ceb3aac011305fa0d3e7812d
aoifeoloughlin <you@example.com>
2026-06-22 13:26:12 +0100

    #01_03 adding logger to other parts

--- reliability_agent/agent/config_loader.py
+++ reliability_agent/agent/config_loader.py
@@ -1,5 +1,6 @@
 import yaml, os, logging
 import pathlib as Path
+from logging_content from get_logger
 
 class ConfigLoader:
     def __init__(self, config_path):
@@ -8,12 +9,14 @@ class ConfigLoader:
         if config_path is None:
             self.config_path = "../reliability_agent/configs/default.yaml" # Getting agent config, if not there default to default
     
+    logger = get_logger()
+
     def load_config(self):
         try:
             with open(self.config_path, 'r') as config:
                 config_yaml = yaml.safe_load(config)
-                logging.info("Config loaded successfully", extra={"config_yaml":config_yaml})
+                logger.info("Config loaded successfully", extra={"config_yaml":config_yaml})
             return config_yaml
         except Exception as e:
-            logging.ERROR("ERROR:", e)
+            logger.ERROR("ERROR:", e)
             traceback.print_exc()
--- reliability_agent/agent/scheduler.py
+++ reliability_agent/agent/scheduler.py
@@ -1,4 +1,5 @@
 import time, logging, signal, threading
+from agent.logging_content import get_logger
 
 class Scheduler:
     def __init__(self, running, interval):
@@ -6,8 +7,10 @@ class Scheduler:
         self.interval = interval
         self.tick_count = 0
     
+    logger = get_logger()
+    
     def futureTask(self):
-        logging.info("Hello from future task")
+        logger.info("Hello from future task")
 
     def run(self, run_thread, stop_thread):
         while not stop_thread.is_set():
@@ -18,10 +21,10 @@ class Scheduler:
                     break
                 if thread:
                     run_thread.clear()
-                    logging.info("Scheduler: Signal received, starting work!")
+                    logger.info("Scheduler: Signal received, starting work!")
                     start=time.monotonic()
                     self.futureTask()  
                     duration = time.monotonic()-start
-                    logging.info(f"Tick {self.tick_count} completed in {duration:.2f}s", extra={"duration":duration, "tick_count":self.tick_count})
+                    logger.info(f"Tick {self.tick_count} completed in {duration:.2f}s", extra={"duration":duration, "tick_count":self.tick_count})
             except Exception as e:
-                logging.error(f"Worker crashed with error: {e}")
+                logger.error(f"Worker crashed with error: {e}")
