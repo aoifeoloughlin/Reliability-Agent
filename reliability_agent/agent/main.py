620d9b9dd257f70beb625d0f280b403019f13bc3
aoifeoloughlin <you@example.com>
2026-06-22 11:34:20 +0100

    #01_03 forgot to import jsonformatter in main

--- reliability_agent/agent/main.py
+++ reliability_agent/agent/main.py
@@ -1,5 +1,5 @@
 import time, signal, logging, threading, os, subprocess, sys
-import agent.json_formatter import JsonFormatter
+from agent.json_formatter import JsonFormatter
 from pathlib import Path
 from agent.scheduler import Scheduler
 from agent.config_loader import ConfigLoader
