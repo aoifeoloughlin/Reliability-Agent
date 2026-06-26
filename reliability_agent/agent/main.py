c795b11b0f528de5f4834ba68146cc6cffbf03b7
aoifeoloughlin <you@example.com>
2026-06-26 05:03:18 +0100

    #01_03 debug: changing where the output will exist

--- reliability_agent/agent/main.py
+++ reliability_agent/agent/main.py
@@ -65,7 +65,7 @@ def main():
     thread.join() #cleans up multi-threading
     logger.info(str(LogEvent.THREAD_ENDED), extra={"time_monotonic":time.monotonic(), "next_run":next_run})
     with open("output.txt", "w") as f:
-        f.write(metric_store)
+        f.write(str(metric_store))
         
 # When running the python command by calling the main directly this is what makes it run
 # This is the entry point
