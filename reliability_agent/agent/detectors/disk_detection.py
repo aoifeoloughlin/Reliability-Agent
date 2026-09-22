cbb4c302121871f18394df425b8045e247f437f0
aoifeoloughlin <you@example.com>
2026-09-22 07:50:57 +0100

    #02_03-adding in detection bones

--- reliability_agent/agent/detectors/disk_detection.py
+++ reliability_agent/agent/detectors/disk_detection.py
@@ -1,6 +1,5 @@
 import shutil, psutil, logging, os
 class DiskDetector:
-    def __init__(self):
     
     def detect(self, yaml_config, disk_data):
         inode_threshold = yaml_config["inode_exhaustion_warning_threshold"]
