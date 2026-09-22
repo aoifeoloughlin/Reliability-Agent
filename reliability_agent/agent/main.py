053e157850c30c8b49753cf76527eaefc1a8add5
aoifeoloughlin <you@example.com>
2026-09-22 07:54:08 +0100

    #02_03-adding in detection bones

--- reliability_agent/agent/main.py
+++ reliability_agent/agent/main.py
@@ -42,9 +42,9 @@ def run_detectors():
 def main():
     global running
     # Load Config
-    agent_config_loader = ConfigLoader("../reliability_agent/configs/thresholds/agent.yaml")
+    agent_config_loader = ConfigLoader("../reliability_agent/configs/agent.yaml")
     agent_config = agent_config_loader.load_config()
-    disk_config_loader = ConfigLoader("../reliability_agent/detectors/disk_thresholds.yaml")
+    disk_config_loader = ConfigLoader("../reliability_agent/detectors/thresholds/disk_thresholds.yaml")
     self.disk_config = disk_config_loader.load_config()
     interval = agent_config["interval_seconds"]
     window_size = agent_config["window_size"]
