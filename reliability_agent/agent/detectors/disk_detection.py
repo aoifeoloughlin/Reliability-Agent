607bc2faf385f8a3c1e39a3f4731f556c0f0214b
aoifeoloughlin <you@example.com>
2026-09-22 08:19:11 +0100

    #02_03-testing detection

--- reliability_agent/agent/detectors/disk_detection.py
+++ reliability_agent/agent/detectors/disk_detection.py
@@ -3,5 +3,5 @@ class DiskDetector:
     
     def detect(self, yaml_config, disk_data):
         inode_threshold = yaml_config["inode_exhaustion_warning_threshold"]
-        print("disk data", disk_data)
+        return disk_data
 
