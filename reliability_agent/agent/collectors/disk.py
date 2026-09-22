fff4a770c2da83101d20d3b9dafdf320171efd53
aoifeoloughlin <you@example.com>
2026-09-22 07:03:48 +0100

    #02_03-add indode perc code

--- reliability_agent/agent/collectors/disk.py
+++ reliability_agent/agent/collectors/disk.py
@@ -4,18 +4,17 @@ class DiskCollector:
         self.usage_stats = []
         
     def collect(self):
+        inode_perc = self.get_inode_perc()
         for partition in psutil.disk_partitions(all=True):
             if partition.mountpoint != '/': # the code trys to get the partition with the root
                 continue
             try:
                 usage = psutil.disk_usage('/')
-                stat = os.statvfs()
-                inode_used = stat.f_files - stat.f_ffree
                 self.usage_stats.append({
                     "device": partition.device,
                     "mount_point": partition.mountpoint,
                     "usage_percent": usage.percent,
-                    "inode_used": inode_used
+                    "inode_perc": inode_perc
                 })
             except PermissionError:
                 logging.warning(
@@ -31,4 +30,10 @@ class DiskCollector:
                 )
                 continue
         return self.usage_stats
+
+    def get_inode_perc(self):
+        stat = os.statvfs('/') # checking used inodes
+        inode_used = stat.f_files-stat.f_ffree
+        total_inode = stat.f_files
+        return (inode_used/total_inode)*100
         
