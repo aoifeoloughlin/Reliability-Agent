3504ae8cd18479261adb1f50bff2ea6c88f841f7
aoifeoloughlin <you@example.com>
2026-09-22 08:31:03 +0100

    #02_03-testing detection

--- reliability_agent/agent/detectors/disk_detection.py
+++ reliability_agent/agent/detectors/disk_detection.py
@@ -1,7 +1,18 @@
-import shutil, psutil, logging, os
+import shutil, psutil, logging, os, json
 class DiskDetector:
     
     def detect(self, yaml_config, disk_data):
-        inode_threshold = yaml_config["inode_exhaustion_warning_threshold"]
-        return disk_data
+        inode_warning_threshold = yaml_config["inode_exhaustion_warning_threshold"]
+        inode_critical_threshold = yaml_config["inode_exhaustion_critical_threshold"]
+        disk = json.loads(disk_data)
+        inode_perc = disk[0]["inode_perc"]
+
+        if inode_perc < inode_threshold:
+            disk_usage_level = "NORMAL"
+        else if inode_perc < inode_warning_threshold and inode_perc > inode_critical_threshold:
+            disk_usage_level = "WARNING"
+        else:
+            disk_usage_level = "CRITICAL"
+
+        return disk_usage_level
 
